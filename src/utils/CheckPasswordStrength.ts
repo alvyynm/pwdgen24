4def8416c8df9e632db75b0c85388c4a754db305
alvyynm <alvyynm@gmail.com>
2024-06-06 13:47:47 +0300

    fix: use check-password-strength package to check for strength

--- src/utils/CheckPasswordStrength.ts
+++ src/utils/CheckPasswordStrength.ts
@@ -1,13 +1,6 @@
-type StrengthType = "WEAK" | "MEDIUM" | "STRONG";
+import { passwordStrength } from "check-password-strength";
+
 export const CheckPasswordStrength = (password: string) => {
-  let passwordStrength: StrengthType = "WEAK";
-  if (password.length < 7) {
-    passwordStrength = "WEAK";
-  } else {
-    //
-    if (new Set(password).size === 1) {
-      passwordStrength = "WEAK";
-    }
-  }
-  return passwordStrength;
+  const strength = passwordStrength(password).value.toUpperCase();
+  return strength;
 };
