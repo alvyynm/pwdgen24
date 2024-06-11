b0c8595eb8d23b2724510c7290b1e04fba065314
alvyynm <alvyynm@gmail.com>
2024-06-11 17:24:11 +0300

    doc: explain what the custom hook does

--- src/hooks/useIsInitialMount.tsx
+++ src/hooks/useIsInitialMount.tsx
@@ -1,8 +1,10 @@
 import { useRef, useEffect } from "react";
 
+// check if the current render is the first one
 const useIsInitialMount = () => {
   const isInitialMount = useRef(true);
 
+  // useEffect sets this ref to false after the first render.
   useEffect(() => {
     isInitialMount.current = false;
   }, []);
